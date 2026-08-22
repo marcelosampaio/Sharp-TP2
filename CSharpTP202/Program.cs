65c4ab26417e40e2099d87e37823f21c62fba6ea
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-08-22 13:15:03 -0300

    feat: Exercício 2

---
+++ CSharpTP202/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CSharpTP202;
+
+class Program
+{
+    static void Main()
+    {
+        DateTime nascimento = LerDataNascimento();
+
+        int dias = CalcularDiasProximoAniversario(nascimento);
+
+        ExibirResultado(dias);
+    }
+
+    static DateTime LerDataNascimento()
+    {
+        Console.Write("Digite sua data de nascimento (dd/MM/yyyy): ");
+
+        DateTime nascimento;
+
+        while (!DateTime.TryParse(Console.ReadLine(), out nascimento) 
+               || nascimento > DateTime.Today)
+        {
+            Console.WriteLine("Data inválida. Digite uma data de nascimento válida.");
+            Console.Write("Digite sua data de nascimento (dd/MM/yyyy): ");
+        }
+
+        return nascimento;
+    }
+
+    static int CalcularDiasProximoAniversario(DateTime nascimento)
+    {
+        DateTime hoje = DateTime.Today;
+
+        DateTime proximoAniversario = new DateTime(
+            hoje.Year,
+            nascimento.Month,
+            nascimento.Day
+        );
+
+        if (proximoAniversario <= hoje)
+        {
+            proximoAniversario = proximoAniversario.AddYears(1);
+        }
+
+        return (proximoAniversario - hoje).Days;
+    }
+
+    static void ExibirResultado(int dias)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Faltam {dias} dias para o próximo aniversário.");
+    }
+}
