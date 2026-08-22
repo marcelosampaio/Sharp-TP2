4c7ed407b8760c2155bef3543d170c39d4dcc760
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-08-22 12:18:39 -0300

    refactor: responsabilidades unicas

--- CSharpTP201/Program.cs
+++ CSharpTP201/Program.cs
@@ -6,51 +6,84 @@ class Program
 {
     static void Main()
     {
+        DateTime nascimento = LerDataNascimento();
+
+        int anos = CalcularAnos(nascimento);
+        int meses = CalcularMeses(nascimento, anos);
+        int dias = CalcularDias(nascimento, anos, meses);
+
+        ExibirResultado(anos, meses, dias);
+    }
+
+    static DateTime LerDataNascimento()
+    {
         Console.Write("Digite sua data de nascimento (dd/MM/yyyy): ");
 
         DateTime nascimento;
 
-        if (DateTime.TryParse(Console.ReadLine(), out nascimento))
+        while (!DateTime.TryParse(Console.ReadLine(), out nascimento))
         {
-            DateTime hoje = DateTime.Today;
-
-            int anos = hoje.Year - nascimento.Year;
-
-            DateTime aniversarioEsteAno = new DateTime(
-                hoje.Year,
-                nascimento.Month,
-                nascimento.Day
-            );
-
-            if (aniversarioEsteAno > hoje)
-            {
-                anos--;
-            }
-
-            DateTime ultimoAniversario = nascimento.AddYears(anos);
-
-            int meses = hoje.Month - ultimoAniversario.Month;
-
-            if (hoje.Day < ultimoAniversario.Day)
-            {
-                meses--;
-            }
-
-            if (meses < 0)
-            {
-                meses += 12;
-            }
-
-            DateTime ultimoMesCompleto = ultimoAniversario.AddMonths(meses);
-
-            int dias = (hoje - ultimoMesCompleto).Days;
-
-            Console.WriteLine();
-            Console.WriteLine($"Idade: {anos} anos, {meses} meses e {dias} dias.");
+            Console.WriteLine("Data inválida. Tente novamente.");
+            Console.Write("Digite sua data de nascimento (dd/MM/yyyy): ");
         }
-        else
+
+        return nascimento;
+    }
+
+    static int CalcularAnos(DateTime nascimento)
+    {
+        DateTime hoje = DateTime.Today;
+
+        int anos = hoje.Year - nascimento.Year;
+
+        DateTime aniversarioEsteAno = new DateTime(
+            hoje.Year,
+            nascimento.Month,
+            nascimento.Day
+        );
+
+        if (aniversarioEsteAno > hoje)
         {
-            Console.WriteLine("Data inválida.");
+            anos--;
         }
+
+        return anos;
+    }
+
+    static int CalcularMeses(DateTime nascimento, int anos)
+    {
+        DateTime hoje = DateTime.Today;
+
+        DateTime ultimoAniversario = nascimento.AddYears(anos);
+
+        int meses = hoje.Month - ultimoAniversario.Month;
+
+        if (hoje.Day < ultimoAniversario.Day)
+        {
+            meses--;
+        }
+
+        if (meses < 0)
+        {
+            meses += 12;
+        }
+
+        return meses;
+    }
+
+    static int CalcularDias(DateTime nascimento, int anos, int meses)
+    {
+        DateTime hoje = DateTime.Today;
+
+        DateTime ultimoAniversario = nascimento.AddYears(anos);
+        DateTime ultimoMesCompleto = ultimoAniversario.AddMonths(meses);
+
+        return (hoje - ultimoMesCompleto).Days;
+    }
+
+    static void ExibirResultado(int anos, int meses, int dias)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Idade: {anos} anos, {meses} meses e {dias} dias.");
     }
 }
